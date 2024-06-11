38510ee8d2971558593bc7045f244dfa9c15cce0
luoyi <luoyi58624@163.com>
2024-06-11 09:21:52 +0800

    chore: 移除外部依赖包

--- lib/luoyi_flutter_base.dart
+++ lib/luoyi_flutter_base.dart
@@ -6,9 +6,6 @@ import 'package:localstorage/localstorage.dart';
 import 'src/utils/session_storage/session_storage.dart';
 
 export 'package:luoyi_dart_base/luoyi_dart_base.dart';
-export 'package:flutter_hooks/flutter_hooks.dart';
-export 'package:gap/gap.dart';
-export 'package:super_sliver_list/super_sliver_list.dart';
 
 // 一个非常轻量的 key-value 本地存储，整个包的源代码不超过100行，不依赖任何本机插件
 export 'package:localstorage/localstorage.dart' show localStorage;
