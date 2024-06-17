da741ce04ac35d5cfcbdec5ec70c32071f0b2c07
luoyi <luoyi58624@163.com>
2024-06-17 09:28:38 +0800

    feat: 移除字体全局配置

--- lib/src/widgets/typography.dart
+++ lib/src/widgets/typography.dart
@@ -2,68 +2,15 @@ import 'package:flutter/widgets.dart';
 import 'package:luoyi_flutter_base/luoyi_flutter_base.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-/// 文字排版模型对象
-class TypographyModel {
-  /// 标题颜色
-  final Color titleColor;
-  final Color titleDarkColor;
+const double _h1FontSize = 28;
+const double _h2FontSize = 24;
+const double _h3FontSize = 20;
+const double _h4FontSize = 18;
+const double _h5FontSize = 16;
+const double _h6FontSize = 14;
+const double _normalFontSize = 15;
 
-  /// 普通文本颜色
-  final Color textColor;
-  final Color textDarkColor;
-
-  /// 超链接文字颜色
-  final Color hrefColor;
-  final Color hrefDarkColor;
-
-  /// 各种类型文本尺寸大小
-  final double h1;
-  final double h2;
-  final double h3;
-  final double h4;
-  final double h5;
-  final double h6;
-  final double p;
-
-  const TypographyModel({
-    this.titleColor = const Color(0xff181818),
-    this.titleDarkColor = const Color(0xfffafafa),
-    this.textColor = const Color(0xff222222),
-    this.textDarkColor = const Color(0xfff2f2f2),
-    this.hrefColor = const Color(0xff0ea5e9),
-    this.hrefDarkColor = const Color(0xff0284c7),
-    this.h1 = 28,
-    this.h2 = 24,
-    this.h3 = 20,
-    this.h4 = 18,
-    this.h5 = 16,
-    this.h6 = 14,
-    this.p = 16,
-  });
-}
-
-/// 配置全局的文字样式
-class TypographyInheritedWidget extends InheritedWidget {
-  const TypographyInheritedWidget({
-    super.key,
-    required super.child,
-    required this.model,
-  });
-
-  final TypographyModel model;
-
-  static TypographyModel of(BuildContext context) {
-    return context
-            .dependOnInheritedWidgetOfExactType<TypographyInheritedWidget>()
-            ?.model ??
-        const TypographyModel();
-  }
-
-  @override
-  bool updateShouldNotify(TypographyInheritedWidget oldWidget) => true;
-}
-
-/// 字体排版抽象类，这里只声明文字通用的属性
+/// 字体排版抽象类
 abstract class _Typography extends StatelessWidget {
   const _Typography(
     this.data, {
@@ -95,11 +42,11 @@ abstract class _Typography extends StatelessWidget {
   final bool italic;
 
   Widget buildTypography(
-    BuildContext context,
-    double size,
-    Color color,
-    FontWeight weight,
-  ) {
+    BuildContext context, {
+    double? size,
+    Color? color,
+    FontWeight? weight,
+  }) {
     TextStyle style = TextStyle(
       fontSize: size,
       color: color,
@@ -135,10 +82,12 @@ abstract class _Title extends _Typography {
     required double size,
     Color? color,
   }) {
-    final model = TypographyInheritedWidget.of(context);
-    final $color =
-        color ?? (context.isDark ? model.titleDarkColor : model.titleColor);
-    return buildTypography(context, size, $color, FontUtil.bold);
+    return buildTypography(
+      context,
+      size: size,
+      color: color,
+      weight: FontUtil.bold,
+    );
   }
 }
 
@@ -160,10 +109,12 @@ abstract class _Text extends _Typography {
     Color? color,
     FontWeight? weight,
   }) {
-    final model = TypographyInheritedWidget.of(context);
-    final $color =
-        color ?? (context.isDark ? model.textDarkColor : model.textColor);
-    return buildTypography(context, size, $color, weight ?? FontUtil.normal);
+    return buildTypography(
+      context,
+      size: size,
+      color: color,
+      weight: weight ?? FontUtil.normal,
+    );
   }
 }
 
@@ -173,8 +124,7 @@ class H1 extends _Title {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    return buildTitle(context, size: size ?? model.h1, color: color);
+    return buildTitle(context, size: size ?? _h1FontSize, color: color);
   }
 }
 
@@ -184,8 +134,7 @@ class H2 extends _Title {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    return buildTitle(context, size: size ?? model.h2, color: color);
+    return buildTitle(context, size: size ?? _h2FontSize, color: color);
   }
 }
 
@@ -195,8 +144,7 @@ class H3 extends _Title {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    return buildTitle(context, size: size ?? model.h3, color: color);
+    return buildTitle(context, size: size ?? _h3FontSize, color: color);
   }
 }
 
@@ -206,8 +154,7 @@ class H4 extends _Title {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    return buildTitle(context, size: size ?? model.h4, color: color);
+    return buildTitle(context, size: size ?? _h4FontSize, color: color);
   }
 }
 
@@ -217,8 +164,7 @@ class H5 extends _Title {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    return buildTitle(context, size: size ?? model.h5, color: color);
+    return buildTitle(context, size: size ?? _h5FontSize, color: color);
   }
 }
 
@@ -228,8 +174,7 @@ class H6 extends _Title {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    return buildTitle(context, size: size ?? model.h6, color: color);
+    return buildTitle(context, size: size ?? _h6FontSize, color: color);
   }
 }
 
@@ -247,10 +192,9 @@ class P extends _Text {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
     return buildText(
       context,
-      size: size ?? model.p,
+      size: size ?? _normalFontSize,
       color: color,
       weight: weight,
     );
@@ -274,9 +218,7 @@ class A extends _Text {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
-    final $color =
-        color ?? (context.isDark ? model.hrefDarkColor : model.hrefColor);
+    final $color = color ?? const Color(0xff409eff);
     return HoverBuilder(builder: (isHover) {
       return TapBuilder(
         onTap: href == null
@@ -287,7 +229,7 @@ class A extends _Text {
         builder: (isTap) {
           return buildText(
             context,
-            size: size ?? model.p,
+            size: size ?? _normalFontSize,
             color: $color
                 .onHover(isHover, $color.deepen(20))
                 .onTap(isTap, $color.deepen(50)),
@@ -312,10 +254,9 @@ class B extends _Text {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
     return buildText(
       context,
-      size: size ?? model.p,
+      size: size ?? _normalFontSize,
       color: color,
       weight: FontUtil.bold,
     );
@@ -335,10 +276,9 @@ class I extends _Text {
 
   @override
   Widget build(BuildContext context) {
-    final model = TypographyInheritedWidget.of(context);
     return buildText(
       context,
-      size: size ?? model.p,
+      size: size ?? _normalFontSize,
       color: color,
       weight: weight,
     );
