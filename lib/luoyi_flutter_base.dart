2fba968a801cd3342cdf18e3ed395cb356bddc55
luoyi <luoyi58624@163.com>
2024-07-12 10:40:44 +0800

    chore: 移除序列化模型

--- lib/luoyi_flutter_base.dart
+++ lib/luoyi_flutter_base.dart
@@ -2,7 +2,6 @@ library luoyi_flutter_base;
 
 export 'package:luoyi_dart_base/luoyi_dart_base.dart';
 
-export 'src/commons/model.dart';
 export 'src/extensions/color.dart';
 export 'src/extensions/int.dart';
 export 'src/utils/flutter.dart';
