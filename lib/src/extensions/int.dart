e3dd10c337a0f33ba5a22a6af2aae2ecbe55285a
luoyi <luoyi58624@163.com>
2024-07-16 10:30:56 +0800

    feat: int扩展函数新增ms

--- lib/src/extensions/int.dart
+++ lib/src/extensions/int.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 extension FlutterIntExtension on int {
+  /// 转换成持续时间对象，单位：毫秒
+  Duration get ms => Duration(milliseconds: this);
+
   /// int转[FontWeight]
   FontWeight toFontWeight([FontWeight? defaultFontWeight]) {
     switch (this) {
