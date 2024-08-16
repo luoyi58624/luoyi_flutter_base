8ae6ab8fcc55f60e3f2e1d6fc0565c5e00f4ce6b
luoyi <luoyi58624@163.com>
2024-08-16 16:31:12 +0800

    feat: 添加 globalTextStyle 扩展函数

--- lib/src/extensions/widget.dart
+++ lib/src/extensions/widget.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/widgets.dart';
 
 import '../widgets/scroll_behavior.dart';
+import '../commons/global.dart' as global;
 
 extension FlutterWidgetExtension on Widget {
   /// 禁止使用祖先定义的默认的滚动条，如果你希望当前滚动小部件不使用滚动条，或者你正在使用自定义滚动条，
   /// 那么只需在小部件的末尾添加这个扩展方法即可
   Widget get noScrollBehavior => ScrollConfiguration(
         behavior: const NoScrollBehavior(),
+        child: this,
+      );
+
+  Widget get globalTextStyle => DefaultTextStyle.merge(
+        style: global.globalTextStyle,
         child: this,
       );
 }
