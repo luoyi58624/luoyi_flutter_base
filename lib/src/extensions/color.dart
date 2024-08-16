451c81cc34cd1b34919bcafc34dfb18688936693
luoyi <luoyi58624@163.com>
2024-08-16 14:57:15 +0800

    feat: color deepen 函数新增 reversal 属性

--- lib/src/extensions/color.dart
+++ lib/src/extensions/color.dart
@@ -37,10 +37,10 @@ extension FlutterColorExtension on Color {
     bool hasAlpha = false,
   }) =>
       '${hasLeading == true ? '#' : ''}'
-          '${hasAlpha == true ? alpha.toRadixString(16).padLeft(2, '0') : ''}'
-          '${red.toRadixString(16).padLeft(2, '0')}'
-          '${green.toRadixString(16).padLeft(2, '0')}'
-          '${blue.toRadixString(16).padLeft(2, '0')}';
+      '${hasAlpha == true ? alpha.toRadixString(16).padLeft(2, '0') : ''}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 
   /// 将当前颜色转换成 Material 颜色
   MaterialColor toMaterialColor() {
@@ -90,14 +90,18 @@ extension FlutterColorExtension on Color {
 
   /// 将颜色变得深，如果当前颜色是亮色，颜色会变暗，但如果当前颜色是暗色，则颜色会变亮
   /// * scale 0-100，值越大，颜色越深
+  /// * reversal 是否反转，如果颜色是亮色，则颜色更亮，否则更暗
   /// * lightScale 0-100，当颜色是亮色时，应用的 scale 值
   /// * darkScale 0-100，当颜色是暗色时，应用的 scale 值
   Color deepen(
-      int scale, {
-        int? lightScale,
-        int? darkScale,
-      }) {
-    return isDark ? brighten(darkScale ?? scale) : darken(lightScale ?? scale);
+    int scale, {
+    bool reversal = false,
+    int? lightScale,
+    int? darkScale,
+  }) {
+    return reversal
+        ? (isDark ? darken(darkScale ?? scale) : brighten(lightScale ?? scale))
+        : (isDark ? brighten(darkScale ?? scale) : darken(lightScale ?? scale));
   }
 
   /// 将当前颜色和另一种颜色按一定比例进行混合
