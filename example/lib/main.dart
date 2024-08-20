b96204e1da627f2c800ffa35ea6f0c1e25afc7e7
luoyi <luoyi58624@163.com>
2024-08-20 16:27:55 +0800

    feat: 新增有序、无序列表

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -35,185 +35,42 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Example'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            P([
-              'xxx',
-              'xxx',
-              'xxx',
-              A(
-                '百度',
-                href: 'https://www.baidu.com',
-                decoration: HrefDecoration.hoverUnderline,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Ol(
+            // start: 1,
+            children: [
+              ...List.generate(5, (index) => const Li(child: '列表')),
+              Ol(
+                children: [
+                  ...List.generate(3, (index) => const Li(child: '列表'))
+                ],
               ),
-              'xxx',
-              'xxx',
-              P(
-                '百度',
-                style: TextStyle(fontSize: 50),
-              ),
-            ]),
-            P([
-              'xxx',
-              P('xxx'),
-              'xxx',
-              Del(
-                B([
-                  '哈喽',
-                  A(
-                    '百度',
-                    href: 'https://www.baidu.com',
-                    color: Colors.grey,
-                  ),
-                  '哈喽',
-                ]),
-              ),
-              'xxx',
-              '百度',
-              H1('一级标题'),
-              Container(
-                width: 50,
-                height: 50,
-                color: Colors.green,
-              )
-            ]),
-            B([
-              '哈喽',
-              A(
-                '百度',
-                href: 'https://www.baidu.com',
-                decoration: HrefDecoration.hoverUnderline,
-              ),
-              '哈喽',
-            ]),
-            P(
-              B([
-                '百度',
-                // A('百度', href: 'https://www.baidu.com'),
-                Text('hal'),
-              ]),
-            ),
-            // test2(),
-            DefaultTextStyle(
-              style: TextStyle(
-                color: Colors.black,
-              ),
-              child: Builder(builder: (context) {
-                return RichText(
-                  text: TextSpan(
-                    style: DefaultTextStyle.of(context).style,
+              Ul(
+                // nestGap: 300,
+                // listStyleType: ListStyleType.none,
+                children: [
+                  ...List.generate(3, (index) => const Li(child: '列表')),
+                  Ul(
+                    listStyleType: ListStyleType.circle,
                     children: [
-                      TextSpan(text: 'hello'),
-                      TextSpan(
+                      ...List.generate(3, (index) => const Li(child: '列表')),
+                      Ul(
+                        listStyleType: ListStyleType.square,
                         children: [
-                          TextSpan(
-                            style: DefaultTextStyle.of(context)
-                                .style
-                                .copyWith(color: Colors.red),
-                            children: [
-                              WidgetSpan(
-                                child: Builder(
-                                  builder: (context) =>
-                                      DefaultTextStyle.merge(child: Text('xx')),
-                                ),
-                              ),
-                              WidgetSpan(
-                                child: Builder(
-                                  builder: (context) => Text('xx'),
-                                ),
-                              ),
-                              TextSpan(text: 'hello'),
-                            ],
-                          ),
+                          ...List.generate(3, (index) => const Li(child: '列表'))
                         ],
                       ),
                     ],
                   ),
-                );
-              }),
-            ),
-          ],
+                ],
+              ),
+              ...List.generate(95, (index) => const Li(child: '列表')),
+            ],
+          ),
         ),
       ),
-    );
-  }
-
-  Widget test2() {
-    return TextWidget(
-      [
-        'xxx',
-        '富文本',
-        'xxx',
-        const TextSpan(
-          text: 'xxx',
-          style: TextStyle(color: Colors.green),
-        ),
-        const TextWidget(
-          'xxx',
-          style: TextStyle(color: Colors.red),
-        ),
-        const TextWidget(
-          ['xxx', '富文本', 'ccc'],
-          style: TextStyle(color: Colors.blue),
-        ),
-        const WidgetSpan(
-          alignment: PlaceholderAlignment.middle,
-          child: Icon(Icons.home),
-        ),
-        // WidgetSpan(
-        //   alignment: PlaceholderAlignment.middle,
-        //   child: Container(
-        //     width: 100,
-        //     height: 100,
-        //     color: Colors.green,
-        //   ),
-        // ),
-        // WidgetSpan(
-        //   alignment: PlaceholderAlignment.baseline,
-        //   baseline: TextBaseline.alphabetic,
-        //   child: Container(
-        //     width: 100,
-        //     height: 100,
-        //     color: Colors.green,
-        //   ),
-        // ),
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.red,
-        ),
-        'xxx',
-        WidgetSpan(
-          alignment: PlaceholderAlignment.baseline,
-          baseline: TextBaseline.alphabetic,
-          child: HoverBuilder(builder: (context) {
-            return TextWidget(
-              'xxx',
-              style: TextStyle(
-                color: HoverBuilder.of(context) ? Colors.red : null,
-              ),
-            );
-          }),
-        ),
-        'xxx',
-        const A(
-          'https://www.baidu.com',
-          href: 'https://www.baidu.com',
-        ),
-        // WidgetSpan(
-        //   child: const ElButton(
-        //     onPressed: null,
-        //     child: '百度',
-        //     link: true,
-        //   ),
-        // ),
-        const TextWidget(
-          ['呃呃呃', '呃呃呃'],
-          style: TextStyle(color: Colors.red),
-        ),
-      ],
-      style: const TextStyle(fontWeight: FontWeight.w500),
     );
   }
 }
