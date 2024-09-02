950ffdbf95339402c195bdf22fe113935af73343
luoyi <luoyi58624@163.com>
2024-09-02 11:59:35 +0800

    fix: eventAnimateDuration -> themeDuration

--- lib/src/extensions/context.dart
+++ lib/src/extensions/context.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 
 import '../../element_ui/components/hover.dart';
 import '../../element_ui/components/tap.dart';
-import '../commons/global.dart';
 
 extension FlutterContextExtension on BuildContext {
   bool get isHover => ElHoverBuilder.of(this);
@@ -28,13 +27,5 @@ extension FlutterContextExtension on BuildContext {
     return result;
   }
 
-  /// 动态调整隐式动画小部件的持续时间，如果是悬停、点击等事件引起的状态更新，则使用自定义的持续时间，
-  /// 否则使用全局主题变化持续时间，这样可以解决切换全局主题时过渡不一致问题
-  Duration eventAnimateDuration(Duration duration) {
-    if (isHover || isTap) {
-      return duration;
-    } else {
-      return GlobalConfig.themeDuration;
-    }
-  }
+
 }
